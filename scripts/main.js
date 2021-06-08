9c91106f54ed66146196115d50a223ab04e6d9bc
Shubhi Jain <shubhi_jain@mccormick.com>
2021-06-08 07:20:07 +0530

    Added Cell Select Facility

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-    $('.title').on("keydown", function(){
-        //action you want taken
-        document.querySelector('title').textContent = document.querySelector('.title').innerHTML.replace(/<\/?[^>]+>/gi, " ").replace('&nbsp;',' ');
-    });
-    setAddressBars();  
+    setAddressBars(); 
     setSheetCells();
-    $('.sheet-cell').dblclick(function(e) {
-        document.getElementsByClassName(`.sheet-cell.active`)[0]?.classList.remove("active")
-        document.getElementById(`${e.target.id}`).classList.add("active");
-        document.getElementById(`${e.target.id}`).setAttribute("contenteditable","true");
+
+    $('.title').on("keydown", function(){
+        updateMetaTitle();
     });
-    $('.sheet-cell').click(function() {
-        $('.sheet-cell.active').removeClass(".active")
+
+    $('.sheet-cell').dblclick(function(e) {
+        activateCell(e)
+    });
+
+    $('.sheet-cell').click(function(e) {
+        deactivateActiveCells(e)
     });
 });
+
+function updateMetaTitle(){
+    document.querySelector('title').textContent = document.querySelector('.title').innerHTML.replace(/<\/?[^>]+>/gi, " ").replace('&nbsp;',' ');
+}
 
 function setAddressBars(){
     for(let n = 0; n < 100; n++){
@@ -33,9 +37,9 @@ function setAddressBars(){
 }
 
 function  setSheetCells(){
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < 20; i++){
         let row = `<div class="sheet-row">`;
-        for(let j = 0; j < 10; j++){
+        for(let j = 0; j < 13; j++){
             let address = '';
             let num = j;
             first = true;
@@ -50,4 +54,26 @@ function  setSheetCells(){
         row += `</div>`;
         $('.sheet-container .sheet').append(row)
     }
+}
+
+function activateCell(e){
+    deactivateActiveCells(e);
+    let cellID = `${e.target.id}`;
+    document.getElementById(cellID).classList.add("active");
+    $(`#${cellID}`).attr("contenteditable","true")
+    let rowID = cellID.split("-")[1];
+    let colID = cellID.split("-")[3];
+    document.getElementById(`row-${rowID}`).classList.add("active");
+    document.getElementById(`col-${colID}`).classList.add("active");
+}
+
+function deactivateActiveCells(e){
+    $(".sheet-cell.active").each(function(){
+        let rowID = this.id.split("-")[1];
+        let colID = this.id.split("-")[3];
+        $(`#${this.id}`).attr("contenteditable","false");
+        document.getElementById(`row-${rowID}`).classList.remove("active");
+        document.getElementById(`col-${colID}`).classList.remove("active");
+    });
+    $(".sheet-cell.active").removeClass("active")
 }
